ede8c15609c9e99a7c26403d9d9c46b2ed5c8e14
MattGueler <mateus.gueler@gmail.com>
2022-08-08 00:56:04 -0300

    Cria layout de acessar shortUrl

---
+++ src/Componets/Open/Open.js
@@ -0,0 +1,56 @@
+import { useState } from "react";
+import { Container } from "../Container/Container";
+import { Input } from "../Input/Input";
+import logo from '../../Assets/IMG/logo.png'
+import { useNavigate } from 'react-router-dom'
+import { Top, Main, Logo, Buttons } from './Open-style'
+import axios from "axios";
+
+function Open() {
+
+    const [url, setUrl] = useState('');
+
+    let navigate = useNavigate()
+
+    function acessUrl(event) {
+
+        event.preventDefault();
+
+        const body = {}
+
+        const URL = `https://project-shortly-16.herokuapp.com/urls/open/${url}`
+
+        const promise = axios.get(URL, body)
+
+        promise.then((res) => {
+            window.location.href = `https://project-shortly-16.herokuapp.com/urls/open/${url}`
+        }).catch((res) => {
+            console.log(res)
+        })
+    }
+
+    return (
+        <Container>
+            <Top>
+                <h2></h2>
+                <Buttons>
+                    <p onClick={() => { navigate('/ranking/geral') }}>Ranking</p>
+                    <p onClick={() => { navigate('/') }}><span>Entrar</span></p>
+                    <p onClick={() => { navigate('/cadastro') }}>Cadastrar-se</p>
+                </Buttons>
+            </Top>
+            <Main>
+                <Logo>
+                    <h1>Shortly</h1>
+                    <img src={logo} />
+                </Logo>
+                <form onSubmit={acessUrl}>
+                    <Input placeholder="Acesse o link" onChange={(e) => { setUrl(e.target.value) }} value={url} />
+                    <button>Acessar</button>
+                </form>
+            </Main>
+        </Container>
+    )
+}
+
+export default Open;
